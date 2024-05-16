84b08802f609cdf272b3b5e50f2476bebe55924a
abcom <abcom@DESKTOP-TCLFLCH>
2024-05-16 11:51:46 +0530

    shift timing update

--- Attendance_API/DTOs/ShiftTimingResponseDTO.cs
+++ Attendance_API/DTOs/ShiftTimingResponseDTO.cs
@@ -5,9 +5,9 @@ namespace Attendance_API.DTOs
     public class ShiftTimingResponseDTO
     {
         public int Shift_Timing_id { get; set; }
-        public TimeSpan Clock_In { get; set; }
-        public TimeSpan Clock_Out { get; set; }
-        public TimeSpan Late_Coming { get; set; }
+        public string Clock_In { get; set; }
+        public string Clock_Out { get; set; }
+        public string Late_Coming { get; set; }
         public DateTime Applicable_Date { get; set; }
         public List<ShiftTimingDesignations> Designations { get; set; }
     }
