8cf5ab69753c723ea3f8301ad4e81210803fedf5
utsavdarji22 <utsavdarji22@gmail.com>
2024-05-22 08:09:37 +0530

    added names

--- Attendance_API/DTOs/EmployeeAttendanceMasterResponseDTO.cs
+++ Attendance_API/DTOs/EmployeeAttendanceMasterResponseDTO.cs
@@ -8,6 +8,8 @@
         public string DepartmentName { get; set; }
         public int? Employee_Attendance_Master_id { get; set; }
         public int? Employee_Attendance_Status_id { get; set; }
+        public string? Employee_Attendance_Status_Type { get; set; }
+        public string? Employee_Attendance_Status_Short_Name { get; set; }
         public string Remarks { get; set; }
     }
 }
