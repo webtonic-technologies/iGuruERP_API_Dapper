1472a15668347fd3ff372772e5d2817bcb037cd7
utsavdarji22 <utsavdarji22@gmail.com>
2024-06-14 01:04:39 +0530

    added validations in attendence api

--- Attendance_API/Models/EmployeeAttendanceStatusMaster.cs
+++ Attendance_API/Models/EmployeeAttendanceStatusMaster.cs
@@ -1,9 +1,13 @@
-﻿namespace Attendance_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Attendance_API.Models
 {
     public class EmployeeAttendanceStatusMaster
     {
         public int Employee_Attendance_Status_id { get; set; }
+        [MaxLength(30)]
         public string Employee_Attendance_Status_Type { get; set; }
+        [MaxLength(15)]
         public string Short_Name { get; set; }
     }
 }
